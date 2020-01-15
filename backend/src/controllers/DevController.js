b4a63ef2ff3ca07217e35439aafaa8f06de0eb95
Filipe Melo <filipe.cauduro@outlook.com>
2020-01-14 23:08:46 -0300

    Criação de todas as funções do DevController.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -7,14 +7,21 @@ const parseStringAsArray = require('../utils/parseStringAsArray');
 module.exports = {
     async index(req, res) {
         const devs = await Dev.find();
-
+        
         return res.json(devs);
     },
+    async show(req, res) {
+        const { github_username } = req.params;
+        
+        const dev = await Dev.findOne({ github_username });
+        
+        return res.json(dev);
+    },
     async store(req, res) {
-        const { github_username, techs, latitude, longitude } = (req.body);
-
+        const { github_username, techs, latitude, longitude } = req.body;
+    
         let dev = await Dev.findOne({ github_username });
-
+    
         if (!dev) { 
             const response = await axios.get(`https://api.github.com/users/${github_username}`);
             
@@ -38,5 +45,40 @@ module.exports = {
         }
     
         return res.json(dev);
-    }
+    },
+    async update(req, res){
+        const { github_username } = req.params;
+        const { techs, latitude, longitude } = req.body;
+        
+        let dev = await Dev.findOne({ github_username });
+        
+        if (dev) {
+            const response = await axios.get(`https://api.github.com/users/${github_username}`);
+            
+            const { name = login, avatar_url, bio } = response.data;
+            
+            const techsArray = parseStringAsArray(techs);
+            
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            };
+            
+            await dev.updateOne({name: name, avatar_url: avatar_url, bio: bio, techs: techsArray, location: location}, {_id: dev._id});
+        }
+        
+        return res.json(dev);
+    },
+    async destroy(req, res){
+        const { github_username } = req.params;
+        const dev = await Dev.findOne({ github_username });
+        
+        if (dev) {
+            dev.deleteOne();
+
+            res.json({ message: "Sucesso" });
+        } else {
+            res.json({ message: "Erro" });
+        }
+    },
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,10 @@ const SearchController = require('./controllers/SearchController');
 const routes = Router();
 
 routes.get('/devs', DevController.index);
+routes.get('/devs/:github_username', DevController.show);
 routes.post('/devs', DevController.store);
+routes.put('/devs/:github_username', DevController.update);
+routes.delete('/devs/:github_username', DevController.destroy);
 
 routes.get('/search', SearchController.index);
 
